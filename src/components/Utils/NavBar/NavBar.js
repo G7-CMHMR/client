572dc92f4de32f4b966470f1585c0fe3357cf062
Natalia Verón <nataliaveronduarte@gmail.com>
2021-07-07 19:03:03 -0300

    nav, nav bar, searchbar

--- src/components/Utils/NavBar/NavBar.js
+++ src/components/Utils/NavBar/NavBar.js
@@ -1,9 +1,43 @@
 import './NavBar.css'
+import {Navbar ,Nav, NavDropdown} from 'react-bootstrap'
+import {FaHeart} from 'react-icons/fa'
+import {IoCartSharp} from 'react-icons/io5'
 
 function NavBar() {
     return (
-        <div className='ContainerNavBar'>
-
+    <div className="ContainerNavBar">
+       <Navbar bg="light" expand="lg">
+        <Navbar.Toggle aria-controls="basic-navbar-nav" />
+         <Navbar.Collapse id="basic-navbar-nav">
+        <Nav id="navcontainer" className="mr-auto">
+         <Nav.Link href="#link">OFERTAS</Nav.Link>
+         <Nav.Link href="#link2">PC ARMADAS</Nav.Link>
+         <NavDropdown title="CATEGORIAS" id="basic-nav-dropdown">
+             <NavDropdown.Item href="#action/3.1">Equipos y Notebooks</NavDropdown.Item>
+             <NavDropdown.Item href="#action/3.2">Procesadores y coolers cpus</NavDropdown.Item>
+             <NavDropdown.Item href="#action/3.3">Memorias Ram</NavDropdown.Item>
+             <NavDropdown.Item href="#action/3.4">Almacenamiento</NavDropdown.Item>
+             <NavDropdown.Item href="#action/3.5">Placas de video</NavDropdown.Item>
+             <NavDropdown.Item href="#action/3.6">Gabinetes </NavDropdown.Item>
+             <NavDropdown.Item href="#action/3.7">Monitores </NavDropdown.Item>
+             <NavDropdown.Item href="#action/3.8">Teclados y mouse </NavDropdown.Item>
+             <NavDropdown.Item href="#action/3.9">Audio </NavDropdown.Item>
+        </NavDropdown>   
+         <Nav.Link href="#login">LOGIN</Nav.Link>
+         <Nav.Link href="#signup">REGISTRATE</Nav.Link>
+         <Nav.Link href="#panel">PANEL</Nav.Link>
+         <NavDropdown title="RAUL" id="basic-nav-dropdown">
+         <NavDropdown.Item href="profile">MI CUENTA </NavDropdown.Item>
+         <NavDropdown.Item href="#fav">MIS FAVORITOS </NavDropdown.Item>
+         <NavDropdown.Item href="historyshop">HISTORIAL DE COMPRAS </NavDropdown.Item>
+         <NavDropdown.Item href="#signout">CERRAR SESIÓN </NavDropdown.Item>
+        </NavDropdown>
+        </Nav>
+        <Nav.Link href="#cart"><IoCartSharp/></Nav.Link>
+        <Nav.Link href="#fav"><FaHeart/></Nav.Link>
+    
+  </Navbar.Collapse>
+</Navbar>
         </div>
     )
 }
--- src/components/Utils/SearchBar/SearchBar.js
+++ src/components/Utils/SearchBar/SearchBar.js
@@ -1,5 +1,24 @@
+import {FormControl, Button, Form} from 'react-bootstrap'
+import {BsSearch} from 'react-icons/bs'
+import './SearchBar.css'
+
 function SearchBar() {
-    
+    return(
+
+      //funcion que busque el input . 
+
+   <div className="SearchBar">
+    <Form className="d-flex">
+      <FormControl id="formcontrol"
+        type="search"
+        placeholder="Search"
+        aria-label="Search"
+      />
+      <Button variant="outline-success"><BsSearch/></Button>
+    </Form>
+   
+ </div>
+    )
 }
 
 
