ad28dc16c184482336473e577c8d115d84ebd9e3
Makeron <44731281+M4K3R0N@users.noreply.github.com>
2021-07-07 10:28:52 -0300

    Add bootstrap to package

--- src/views/home/home.js
+++ src/views/home/home.js
@@ -11,8 +11,13 @@ function Home() {
                 <div id='PhotosHome'>
                     <h1>Quiz and ArmarPC</h1>
                 </div>
-                <div id='CarrouselHome'>
-                    <h1>Carrousel</h1>
+                <div id='CarrouselHome' class='carousel slide' data-ride='carousel'>
+                    <div class="carousel-indicators">
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+                        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+                    </div>
+            
                 </div>
                 <div id='CardsHome'>
                     <h1>ProductCards</h1>
@@ -27,3 +32,7 @@ function Home() {
 
 
 export default Home
+
+
+
+
