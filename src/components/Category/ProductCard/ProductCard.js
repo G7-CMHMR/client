2ad70e21f49c15704e8844d880999af8fa79f20a
Natalia Verón <nataliaveronduarte@gmail.com>
2021-07-09 16:27:05 -0300

    Producto de prueba

--- src/components/Category/ProductCard/ProductCard.js
+++ src/components/Category/ProductCard/ProductCard.js
@@ -5,41 +5,29 @@ import {FaHeart} from 'react-icons/fa'
 import {IoCartSharp} from 'react-icons/io5'
 import { Link } from 'react-router-dom';
 
-var prueba= {
-    titulo:"Producto de prueba",
-    precio:4354,
-    precioviejo:10000,
-    vendedor:"Fulanito",
-    estado:"Nuevo",
-    freeship:true,
-    stars:3.5,
-    imagen:"https://www.fullh4rd.com.ar/img/productos/Pics_Prod/mouse-logitech-g203-0.jpg"
-}
 
-
-function ProductCard() {
-    var porcentaje= (prueba.precio / prueba.precioviejo)*100;
+function ProductCard({precio,precioviejo,imagen,titulo,vendedor,estado,stars,freeship}) {
+    var porcentaje= (precio / precioviejo)*100;
     var intPorcentaje = 100-(Math.round( porcentaje ))+"%";
-
 
     return(
         <div id="ProductCard">
            <div id="container">	
 	            <div class="product-image">
-                	<img src={prueba.imagen} alt="Omar Dsoky"/>
+                	<img src={imagen} alt="Omar Dsoky"/>
                 </div>
                 <Link id="link" to="/">
                 	<div class="product-details">
-	                    <h1>{prueba.titulo}</h1>
-                		<p id="seller">Vendido por {prueba.vendedor}</p>
-                        <div id="price"><h3>${prueba.precio}</h3><span>${prueba.precioviejo}</span></div>
-	                	<p class="information">{prueba.estado}</p>
-                      <Rating name="half-rating-read" defaultValue={prueba.stars} precision={0.5} readOnly />
+	                    <h1>{titulo}</h1>
+                		<p id="seller">Vendido por {vendedor}</p>
+                        <div id="price"><h3>${precio}</h3><span>${precioviejo}</span></div>
+	                	<p class="information">{estado}</p>
+                      <Rating name="half-rating-read" defaultValue={stars} precision={0.5} readOnly />
 		            </div>
                 </Link>
                 <div id="offandship">
-                        { prueba.freeship? <div id="ship">Envio gratis</div> :  <p></p> }
-                       { prueba.precioviejo? <div id="off">{intPorcentaje} OFF</div> : <p></p> }
+                        { freeship? <div id="ship">Envio gratis</div> :  <p></p> }
+                       { precioviejo? <div id="off">{intPorcentaje} OFF</div> : <p></p> }
                        
                 </div>
                 <div id="icons">
@@ -47,9 +35,7 @@ function ProductCard() {
                     <button id="btncart"><IoCartSharp/></button>
                 </div>
 	    </div>
-    </div>
-            
-           
+    </div>    
         )
     }
 
