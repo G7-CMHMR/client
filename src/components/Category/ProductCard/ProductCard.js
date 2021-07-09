dfc5a6ab07d27646d58ddb5610db19dd193e943c
Natalia Verón <nataliaveronduarte@gmail.com>
2021-07-09 16:08:07 -0300

    Card product fix

--- src/components/Category/ProductCard/ProductCard.js
+++ src/components/Category/ProductCard/ProductCard.js
@@ -7,7 +7,7 @@ import { Link } from 'react-router-dom';
 
 var prueba= {
     titulo:"Producto de prueba",
-    precio:34354,
+    precio:4354,
     precioviejo:10000,
     vendedor:"Fulanito",
     estado:"Nuevo",
@@ -18,25 +18,34 @@ var prueba= {
 
 
 function ProductCard() {
+    var porcentaje= (prueba.precio / prueba.precioviejo)*100;
+    var intPorcentaje = 100-(Math.round( porcentaje ))+"%";
+
+
     return(
         <div id="ProductCard">
            <div id="container">	
 	            <div class="product-image">
                 	<img src={prueba.imagen} alt="Omar Dsoky"/>
                 </div>
-                <Link to="/">
+                <Link id="link" to="/">
                 	<div class="product-details">
 	                    <h1>{prueba.titulo}</h1>
-                		<p class="information">{prueba.estado}</p>
-                        <h3>${prueba.precio}</h3>
-	                	<p id="seller">Vendido por {prueba.vendedor}</p>
+                		<p id="seller">Vendido por {prueba.vendedor}</p>
+                        <div id="price"><h3>${prueba.precio}</h3><span>${prueba.precioviejo}</span></div>
+	                	<p class="information">{prueba.estado}</p>
                       <Rating name="half-rating-read" defaultValue={prueba.stars} precision={0.5} readOnly />
 		            </div>
                 </Link>
+                <div id="offandship">
+                        { prueba.freeship? <div id="ship">Envio gratis</div> :  <p></p> }
+                       { prueba.precioviejo? <div id="off">{intPorcentaje} OFF</div> : <p></p> }
+                       
+                </div>
                 <div id="icons">
                      <button id="btnheart"><FaHeart/></button>
                     <button id="btncart"><IoCartSharp/></button>
-            </div>
+                </div>
 	    </div>
     </div>
             
