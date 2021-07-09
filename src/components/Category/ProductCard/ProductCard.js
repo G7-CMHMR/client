54003cd16eb7a17b56b603f7ece3dec32597d93b
Natalia Verón <nataliaveronduarte@gmail.com>
2021-07-09 01:52:30 -0300

    Product card stars

--- src/components/Category/ProductCard/ProductCard.js
+++ src/components/Category/ProductCard/ProductCard.js
@@ -1,3 +1,4 @@
+//card dentro de Categorias, REsultados, favoritos
 import './ProductCard.css'
 import Rating from '@material-ui/lab/Rating';
 import {FaHeart} from 'react-icons/fa'
@@ -18,32 +19,25 @@ var prueba= {
 function ProductCard() {
     return(
         <div id="ProductCard">
-           
-            <div id="container">	
-	<div class="product-image">
-	<img src="https://www.fullh4rd.com.ar/img/productos/Pics_Prod/mouse-logitech-g203-0.jpg" alt="Omar Dsoky"/>
-
-</div><Link to="/">
-	<div class="product-details">
-		
-	<h1>{prueba.titulo}</h1>
-
-		<p class="information">{prueba.estado}</p>
-        <h3>${prueba.precio}</h3>
-		<p id="seller">Vendido por {prueba.vendedor}</p>
-        <Rating name="half-rating-read" defaultValue={prueba.stars} precision={0.5} readOnly />
-			
-</div></Link>
-<div id="icons">
-    <button id="btnheart"><FaHeart/></button>
-    <button id="btncart"><IoCartSharp/></button>
-    
-</div>
-			
-</div>
-
-
-</div>
+           <div id="container">	
+	            <div class="product-image">
+                	<img src={prueba.imagen} alt="Omar Dsoky"/>
+                </div>
+                <Link to="/">
+                	<div class="product-details">
+	                    <h1>{prueba.titulo}</h1>
+                		<p class="information">{prueba.estado}</p>
+                        <h3>${prueba.precio}</h3>
+	                	<p id="seller">Vendido por {prueba.vendedor}</p>
+                      <Rating name="half-rating-read" defaultValue={prueba.stars} precision={0.5} readOnly />
+		            </div>
+                </Link>
+                <div id="icons">
+                     <button id="btnheart"><FaHeart/></button>
+                    <button id="btncart"><IoCartSharp/></button>
+            </div>
+	    </div>
+    </div>
             
         )
     }
