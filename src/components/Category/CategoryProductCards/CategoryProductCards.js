19292361afc6dfcf663653bd44582ae2e44a7b6a
Natalia Verón <80424181+nativeron@users.noreply.github.com>
2021-07-09 17:06:56 -0300

    Fix card and carousel (#9)
    * carousel fix
    
    * Card product fix
    
    * Producto de prueba

--- src/components/Category/CategoryProductCards/CategoryProductCards.js
+++ src/components/Category/CategoryProductCards/CategoryProductCards.js
@@ -2,14 +2,39 @@ import CategoryOrder from '../CategoryOrder/CategoryOrder'
 import ProductCard from '../ProductCard/ProductCard'
 import './CategoryProductCards.css'
 
+var prueba= [{
+    titulo:"Producto de prueba",
+    precio:4354,
+    precioviejo:10000,
+    vendedor:"Fulanito",
+    estado:"Nuevo",
+    freeship:true,
+    stars:3.5,
+    imagen:"https://www.fullh4rd.com.ar/img/productos/Pics_Prod/mouse-logitech-g203-0.jpg"
+},
+{
+    titulo:"Producto de prueba 2",
+    precio:201,
+    precioviejo:1000,
+    vendedor:"Pirulo",
+    estado:"Racondicionado",
+    freeship:false,
+    stars:5,
+    imagen:"https://http2.mlstatic.com/D_NQ_NP_948354-MLA45141563332_032021-W.jpg"
+}]
+
 function CategoryProductCards() {
     return(
         <div id="CategoryProductCards">
             <CategoryOrder></CategoryOrder>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>
-            <ProductCard></ProductCard>  
-            
+            {prueba.map((x)=>{
+			return(
+				<ProductCard titulo={x.titulo} imagen={x.imagen} 
+                stars={x.stars} freeship={x.freeship} precio={x.precio}
+                precioviejo={x.precioviejo} vendedor={x.vendedor}
+                estado={x.estado} />
+			)
+		})}
         </div>
     )
 }
--- src/components/Category/ProductCard/ProductCard.js
+++ src/components/Category/ProductCard/ProductCard.js
@@ -5,42 +5,37 @@ import {FaHeart} from 'react-icons/fa'
 import {IoCartSharp} from 'react-icons/io5'
 import { Link } from 'react-router-dom';
 
-var prueba= {
-    titulo:"Producto de prueba",
-    precio:34354,
-    precioviejo:10000,
-    vendedor:"Fulanito",
-    estado:"Nuevo",
-    freeship:true,
-    stars:3.5,
-    imagen:"https://www.fullh4rd.com.ar/img/productos/Pics_Prod/mouse-logitech-g203-0.jpg"
-}
 
+function ProductCard({precio,precioviejo,imagen,titulo,vendedor,estado,stars,freeship}) {
+    var porcentaje= (precio / precioviejo)*100;
+    var intPorcentaje = 100-(Math.round( porcentaje ))+"%";
 
-function ProductCard() {
     return(
         <div id="ProductCard">
            <div id="container">	
 	            <div class="product-image">
-                	<img src={prueba.imagen} alt="Omar Dsoky"/>
+                	<img src={imagen} alt="Omar Dsoky"/>
                 </div>
-                <Link to="/">
+                <Link id="link" to="/">
                 	<div class="product-details">
-	                    <h1>{prueba.titulo}</h1>
-                		<p class="information">{prueba.estado}</p>
-                        <h3>${prueba.precio}</h3>
-	                	<p id="seller">Vendido por {prueba.vendedor}</p>
-                      <Rating name="half-rating-read" defaultValue={prueba.stars} precision={0.5} readOnly />
+	                    <h1>{titulo}</h1>
+                		<p id="seller">Vendido por {vendedor}</p>
+                        <div id="price"><h3>${precio}</h3><span>${precioviejo}</span></div>
+	                	<p class="information">{estado}</p>
+                      <Rating name="half-rating-read" defaultValue={stars} precision={0.5} readOnly />
 		            </div>
                 </Link>
+                <div id="offandship">
+                        { freeship? <div id="ship">Envio gratis</div> :  <p></p> }
+                       { precioviejo? <div id="off">{intPorcentaje} OFF</div> : <p></p> }
+                       
+                </div>
                 <div id="icons">
                      <button id="btnheart"><FaHeart/></button>
                     <button id="btncart"><IoCartSharp/></button>
-            </div>
+                </div>
 	    </div>
-    </div>
-            
-           
+    </div>    
         )
     }
 
