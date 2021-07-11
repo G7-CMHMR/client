0abbfabae95469044f08122483096109213cacd8
Matias Bloisi <44731281+MB-ARG@users.noreply.github.com>
2021-07-11 20:27:58 -0300

    Add cards in home and add Categories List (#20)
    Co-authored-by: Makeron <44731281+M4K3R0N@users.noreply.github.com>

--- src/redux/Actions/Products/Reducer.js
+++ src/redux/Actions/Products/Reducer.js
@@ -1,16 +1,21 @@
-import {GET_PRODUCTS, GET_PRODUCT_DETAIL, SORT} from './ActionsName'
+import { GET_PRODUCTS, GET_PRODUCT_DETAIL, GET_PRODUCTS_OFFER, GET_CATEGORIES, SORT } from './ActionsName'
+
 
 const initialState = {
     products: [],
+    productsOffer: [],
     productDetail: undefined,
+    categories: [],
+
 }
 
-function productsReducer (state = initialState, action) {
-    switch (action.type){
+function productsReducer(state = initialState, action) {
+    switch (action.type) {
         case GET_PRODUCTS: {
             return {
                 ...state,
                 products: action.payload
+                
             }
         }
         case GET_PRODUCT_DETAIL: {
@@ -18,6 +23,21 @@ function productsReducer (state = initialState, action) {
                 ...state,
                 productDetail: action.payload
             }
+
+        }
+        case GET_PRODUCTS_OFFER: {
+            return {
+                ...state,
+                productsOffer: action.payload
+            }
+        }
+        case GET_CATEGORIES: {
+            return {
+                ...state,
+                categories: action.payload
+            }
+        }
+
         } 
         case SORT: {
             return {
@@ -27,7 +47,7 @@ function productsReducer (state = initialState, action) {
         }          
         default: return state;
     }
-    
+
 }
 
 export default productsReducer
