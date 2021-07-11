386ce51f9de65192fe9545270d74a052319aa968
Natalia Verón <nataliaveronduarte@gmail.com>
2021-07-11 20:50:35 -0300

    fix ordenamiento

--- src/redux/Actions/Products/Actions.js
+++ src/redux/Actions/Products/Actions.js
@@ -23,8 +23,20 @@ export function getProductDetail(id) {
 export function sort(order, array){
     let sortArray = [...array]
     sortArray.sort(function (a, b) {
-        var a = parseFloat(a.price)
-        var b = parseFloat(b.price)
+
+        function priceToDisplay (x){
+            let price = x.price
+            if (x.discount>0){price =Math.floor(x.price*171 - (x.price*171/100 )*x.discount)}
+            else { price = Math.floor(price*171) }  
+            return price 
+        }
+//(Math.floor(price*171 - (price*171/100)*discount))
+
+//{props.discount && props.discount > 0 && show ? <h4 style={{textDecoration:"line-through"}}
+//${addCommas(Math.floor(props.price*171))}</h4> : show && <br/>}
+//(props.price*171 - (props.price*171/100)*props.discount)
+        var a = parseFloat(priceToDisplay(a))
+        var b = parseFloat(priceToDisplay(b))
         if (order === 'minmax') {
             if (a < b) { return -1 }
             if (a > b) { return 1  }
