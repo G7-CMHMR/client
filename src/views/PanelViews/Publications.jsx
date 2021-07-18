0796207fa5808f782b2ac023a33eeff108f169e3
Natalia Verón <nataliaveronduarte@gmail.com>
2021-07-18 02:57:23 -0300

    Mis publicaciones, card hechas

--- src/views/PanelViews/Publications.jsx
+++ src/views/PanelViews/Publications.jsx
@@ -1,29 +1,34 @@
 import Separate from '../../components/Utils/Separate/Separate';
-import './Publications.css'
-
-
-
 import React, { useDispatch, useSelector } from 'react-redux';
 import { useEffect } from 'react';
 import { useParams } from "react-router";
-
-import Menu from '../../components/Panel/Menu/Menu';
+import MyPublicationCard from '../../components/Utils/MyPublicationCard/MyPublicationCard'
+import { getFavourites } from '../../redux/Actions/Favourites/Actions';
 
 function Publications() {
-
-    //const dispatch = useDispatch();
-    //const productsReducer = useSelector(state => state.productsReducer)
-
-
-    // useEffect(() => {
-    //   //dispatch(getProductDetail(idProducto))
-    // }, [dispatch, idProducto])
-
+    const userReducer = useSelector(state => state.userReducer.userData)
+    const dispatch = useDispatch();
+    var userID = userReducer.id
+    const favourites = useSelector(state => state.favouritesReducer.favourites)
+    useEffect(() => {
+        dispatch(getFavourites(userID))
+      }, [ dispatch, userID])
 
     return (
-
-        <div className='PanelContainer'>
-            <h1>Publications</h1>
+    
+        <div className='ProductPanelContainer'>
+            <h1>Mis publicaciones</h1>
+            <h6>esta mostrando favoritos, no mis publicaciones</h6>
+            <br></br>
+                <br></br>
+                {
+                    favourites.length>0 ? favourites.map((x) => {
+                        return <MyPublicationCard
+                        price={x.price} discount={x.discount} images={x.images}
+                         name={x.name} id={x.id} stock={x.stock} sold={x.sold}
+                        /> 
+                    }): <p>Ups no tenes publicaciones! Activa uno de tus productos o bien, crea uno</p>
+                }
         </div>
     )
 }
