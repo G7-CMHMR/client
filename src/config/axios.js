f0361bd64fea88efc4ca12c051cff412266f0159
Nicolas Bovina Martijena <nicobovina@gmail.com>
2021-07-19 16:45:54 -0300

    Modificado config/axios.js
    Modificada para que tenga la opcion de hacer las peticiones en produccion o desarrollo

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-    baseURL: 'https://pg-cmhmr-api.herokuapp.com/'
+    baseURL: 'http://localhost:3001'
+//    baseURL: 'https://pg-cmhmr-api.herokuapp.com/'
 });
 export default clientAxios;
