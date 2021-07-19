e691a9a93c1d236f329f7b68d0025f6dc3b549c3
Nicolas Bovina Martijena <nicobovina@gmail.com>
2021-07-19 13:32:36 -0300

    Update axios.js

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const clientAxios = axios.create({
-    baseURL: 'http://localhost:3001'
+    baseURL: 'https://pg-cmhmr-api.herokuapp.com/'
 });
 export default clientAxios;
