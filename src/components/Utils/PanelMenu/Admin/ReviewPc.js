203205856ecb8703912602468a068680fda3d555
Natalia Verón <80424181+nativeron@users.noreply.github.com>
2021-07-29 13:44:55 -0300

    panel review pc (#113)

--- src/components/Utils/PanelMenu/Admin/ReviewPc.js
+++ src/components/Utils/PanelMenu/Admin/ReviewPc.js
@@ -1,23 +1,27 @@
 import React, { useEffect } from 'react'
 import { useDispatch, useSelector } from 'react-redux'
+import { GetPcNotValidate } from '../../../../redux/Actions/Admin/Actions'
 import { getProductsOfCategory } from '../../../../redux/Actions/Products/Actions'
 import CardPC from './CardPC'
 
 export default function ReviewPc() {
     const dispatch = useDispatch()
     const products = useSelector(state => state.productsReducer.products)
+    const pc = useSelector(state => state.adminReducer.pc)
+    const user = useSelector(state => state.userReducer.userData)
+    var iduser= user.id
     useEffect(() => {
-        dispatch(getProductsOfCategory('Procesador'))
+        dispatch(GetPcNotValidate())
     }, [dispatch])
     return (
         <div>
             
-            <h5>se esta msotrando procesador pero se mostraria lista de PC que haria falta evaluar. (Para quiz)</h5>
-            <div id="ReviewContainer" style={{display:'flex',flexWrap:'wrap',justifyContent:'space-around'}}>
+        <div id="ReviewContainer" >
             {
-                products.map((x)=>{
-                    return <CardPC id={x.id} name={x.name} images={x.images} description={x.description}/>
-                })
+                pc && pc[0] ? pc[0].products.map((x)=>{
+                    return <CardPC id={x.id} name={x.name} images={x.images} 
+                    description={x.description} brand={x.name} type={x.type} seller={x.seller}/>
+                }) : ''
             }
             </div>
         </div>
