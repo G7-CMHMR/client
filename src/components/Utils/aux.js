4eaab2d43b2616f4d2baf60c71ca2d8ca46615ee
Matias Bloisi <bloisimatias@hotmail.com>
2021-07-30 12:30:44 -0300

    Revert "Quiz y css (#126)" (#129)
    This reverts commit 9924c99fd0ac2a84ad3a79a8b3b542520a3f7936.

--- src/components/Utils/aux.js
+++
@@ -1,8 +0,0 @@
-export function Objectfilter(object){
-    let array=[]
-    for (const property in object) {
-        if (object[property]===true){
-            array.push(property)
-        }
-      }return Math.max(...array)
-    }
